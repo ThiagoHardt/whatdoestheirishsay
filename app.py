d629d924d949bb59f5445b9f50d3e2ef55563903
ThiagoHardt <thiago.hardts@gmail.com>
2021-03-05 17:16:30 +0000

    game page added and fixed links

--- app.py
+++ app.py
@@ -20,6 +20,21 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/play")
+def play():
+    return render_template("game_page.html")
+
+
+@app.route("/rules")
+def rules():
+    return render_template("rules.html")
+
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
