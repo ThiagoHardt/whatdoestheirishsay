17f8ffaaa34f68669721446db145b3782622ff2c
Thiago Hardt <thiago.hardts@gmail.com>
2021-03-06 13:09:12 +0000

    Added main play functionality

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
+import six
+import random
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from google.cloud import translate_v2 as translate
 
 if os.path.exists("env.py"):
     import env
@@ -24,17 +27,49 @@ def index():
 
 @app.route("/play")
 def play():
-    return render_template("game_page.html")
+
+    targetLanguage = "ga"
+    randomWordsList = getRandomWordsList()
+    randomWord = random.choice(randomWordsList)
+
+    translatedWord = translate_text(targetLanguage, randomWord)
+
+    return render_template("game_page.html", translatedWord=translatedWord, randomWordsList=randomWordsList)
 
 
-@app.route("/rules")
+@ app.route("/rules")
 def rules():
     return render_template("rules.html")
 
 
-@app.route("/leaderboard")
+@ app.route("/leaderboard")
 def leaderboard():
     return render_template("leaderboard.html")
+
+
+def getRandomWordsList():
+
+    randomWordsList = []
+    while len(randomWordsList) < 4:
+        randomWordsList.append(random.choice(
+            open('words.txt').read().split()).strip())
+
+    return randomWordsList
+
+
+def translate_text(target, text):
+
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    result = translate_client.translate(text, target_language=target)
+
+    word = ""
+    word = result
+
+    return word
 
 
 if __name__ == "__main__":
