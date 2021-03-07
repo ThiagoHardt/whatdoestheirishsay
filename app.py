816f0d08fbab974c929325f64c1778d2847825a1
Thiago Hardt <thiago.hardts@gmail.com>
2021-03-07 15:50:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,10 @@ def rules():
 def leaderboard():
     return render_template("leaderboard.html")
 
+@ app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 def getRandomWordsList():
     """ returns a list of random words from the words.txt file """
